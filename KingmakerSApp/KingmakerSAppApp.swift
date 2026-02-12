e7a6cd10c2eb7ba6cd27e89614183afc76f9a0ac
Maksim Kosharny <maksimkosharnyjj@gmail.com>
2026-02-12 17:18:45 +0200

    Initial Commit

---
+++ KingmakerSApp/KingmakerSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KingmakerSAppApp.swift
+//  KingmakerSApp
+//
+//  Created by Maksim Kosharny on 12.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct KingmakerSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
